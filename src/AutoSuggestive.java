02c0a54635616fc6ae5baa924b823f20c370d117
Edenilson Ruiz <ruiz.edenilson@gmail.com>
2022-10-16 20:51:25 -0600

    started AutoSuggestive

--- src/AutoSuggestive.java
+++ src/AutoSuggestive.java
@@ -1,8 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AutoSuggestive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:/ToolsWebDev/chromedriver/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-gpu", "--window-size=1920,1080", "--ignore-certificate-errors",
+				"--disable-extensions", "--no-sandbox", "--disable-dev-shm-usage");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
 	}
 
